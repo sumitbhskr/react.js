57e2a2a4f446f247eb608fd60302d394314a2272
sumitbhskr <sumitbhaskar430@gmail.com>
2025-10-29 14:35:52 +0530

    react css commit

---
+++ 05-Css/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './header.module.css'
+
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <h3>Sumit</h3>
+      <button>Login</button>
+    </div>
+  )
+}
+
+export default Header
