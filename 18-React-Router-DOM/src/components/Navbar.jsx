8007841a3bf6295ac3f6c316183efb04a97ef990
sumitbhskr <sumitbhaskar430@gmail.com>
2025-10-31 07:02:43 +0530

    initial  commit

---
+++ 18-React-Router-DOM/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+    import React from 'react'
+    import { Link } from 'react-router-dom'
+    
+    const Navbar = () => {
+      return (
+        
+          <div className='nav'>
+        <h3>Bhaskar</h3>
+        <div>
+         <Link  to= '/'>Home</Link>
+         <Link  to= '/about'>About</Link>
+         <Link  to= '/contact'>Contact</Link>
+         <Link  to= '/product'>Product</Link>
+        </div>
+      </div>
+    
+      )
+    }
+    
+    export default Navbar
+    
